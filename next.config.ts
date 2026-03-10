ff7398af93c0152c74bc12a3679d9ce444e0f806
Visible <154663548+TrueV1sion@users.noreply.github.com>
2026-03-10 16:56:36 -0400

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  serverExternalPackages: ["@modelcontextprotocol/sdk"],
+  serverExternalPackages: ["@modelcontextprotocol/sdk", "@anthropic-ai/sdk"],
 };
 
 export default nextConfig;
